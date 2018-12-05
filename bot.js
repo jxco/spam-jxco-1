8e1b9a2aa1dd78a06b117c7a2b5c5a419fdd0365
jxco <43864779+jxco@users.noreply.github.com>
2018-12-05 03:01:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,25 +23,4 @@ client.on('message', message => {
     }
     });
 
-client.on('ready',async () => {
-  let GUILDID = '519658926002798622';
-  let CHANNELID = '519658926002798623'; 
-  voiceStay(GUILDID, CHANNELID);
-  function voiceStay(guildid, channelid) {
-    if(!guildid) throw new Error('Syntax: voiceStay function requires guildid');
-    if(!channelid) throw new Error('Syntax: voiceStay function requires channelid');
-
-    let guild = client.guilds.get(guildid);
-    let channel = guild.channels.get(channelid);
-
-    if(channel.type === 'voice') {
-      channel.join().catch(e => {
-        console.log(`Failed To Join :: ${e.message}`);
-      });
-    } else {
-      console.log(`Channel Type :: ${channel.type}, It must be Voice.`);
-    }
-  }
-});
-
 client.login(process.env.BOT_TOKEN);
